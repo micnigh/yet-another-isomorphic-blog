48fb8fed94eb7af70715bb1f6af9cbf53748103d
Michael Nigh <mic.nigh@gmail.com>
2015-10-18 19:35:26 -0700

    Apply active class in navigation for `home` page

--- shared/component/Navigation.js
+++ shared/component/Navigation.js
@@ -1,12 +1,12 @@
 var React = require("react");
-import { Link } from "react-router";
+import { Link, IndexLink } from "react-router";
 var { baseUrl } = require("../baseUrl");
 
 class Navigation extends React.Component {
   render () {
     return (
       <div className="navigation">
-        <Link to={`${baseUrl}`} activeClassName="active" className="brand">Yet Another Isomorphic Blog</Link>
+        <IndexLink to={`${baseUrl}`} activeClassName="active" className="brand">Yet Another Isomorphic Blog</IndexLink>
       </div>
     );
   }
