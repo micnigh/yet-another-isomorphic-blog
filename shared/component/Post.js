6f7420f4c1cec6c8aaa9c378074d2f9e2b4a26d6
Michael Nigh <mic.nigh@gmail.com>
2015-10-25 11:50:29 -0700

    Animate from left or right, revert to default anim on complete

--- shared/component/Post.js
+++ shared/component/Post.js
@@ -11,8 +11,8 @@ class Post extends React.Component {
         tags: tags,
       }, prev, next
     } = this.props;
-    var prevLinkEl = prev === null ? <span className="prev">prev</span> : <span onClick={() => this.props.setTransitionName("in-from-left") }><Link to={`${baseUrl}posts/${prev.slug}`} className="prev" setTransitionName={this.props.setTransitionName}>prev - {prev.title}</Link></span>;
-    var nextLinkEl = next === null ? <span className="next">next</span> : <span onClick={() => this.props.setTransitionName("in-from-right") }><Link to={`${baseUrl}posts/${next.slug}`} className="next" setTransitionName={this.props.setTransitionName}>{next.title} - next</Link></span>;
+    var prevLinkEl = prev === null ? <span className="prev">prev</span> : <span onClick={() => this.props.setTransitionName("in-from-left") }><Link to={`${baseUrl}posts/${prev.slug}`} className="prev">prev - {prev.title}</Link></span>;
+    var nextLinkEl = next === null ? <span className="next">next</span> : <span onClick={() => this.props.setTransitionName("in-from-right") }><Link to={`${baseUrl}posts/${next.slug}`} className="next">{next.title} - next</Link></span>;
     var tagEls = tags.map(t => <Link to={`${baseUrl}tag/${t}`} key={t} className="tag"> {t} </Link>);
     return (
       <div className="post">
--- shared/page/LayoutPage.js
+++ shared/page/LayoutPage.js
@@ -3,12 +3,15 @@ var ReactCSSTransitionGroup = require("react-addons-css-transition-group");
 
 var Navigation = require("../component/Navigation");
 
+var defaultState = {
+  transitionName: "in-from-right",
+  transitionDuration: 1000,
+};
+
 class PageLayout extends React.Component {
   constructor (props) {
     super(props);
-    this.state = {
-      transitionName: "in-from-right",
-    };
+    this.state = Object.assign({}, defaultState);
     this.setTransitionName.bind(this);
   }
 
@@ -18,7 +21,7 @@ class PageLayout extends React.Component {
     return (
       <div>
         <Navigation/>
-        <ReactCSSTransitionGroup transitionName={ this.state.transitionName } transitionEnterTimeout={1000} transitionLeaveTimeout={1000} component="div" className="animation-wrapper">
+        <ReactCSSTransitionGroup transitionName={ this.state.transitionName } transitionEnterTimeout={ this.state.transitionDuration } transitionLeaveTimeout={ this.state.transitionDuration } component="div" className="animation-wrapper">
           { React.cloneElement(this.props.children, { key: pathname, setTransitionName: (transitionName) => {
             self.setTransitionName(transitionName);
           } }) }
@@ -29,6 +32,10 @@ class PageLayout extends React.Component {
 
   setTransitionName(transitionName) {
     this.setState({transitionName});
+    // revert animation to default when done
+    setTimeout(() => {
+      this.setState({transitionName: defaultState.transitionName});
+    }, this.state.transitionDuration);
   }
 }
 
